ed4e6be96166db0e501818de0a37ecc103229946
Allen <yx_endeavor@163.com>
2019-12-08 20:46:43 +0800

    修改代码

--- code/hello-spring-cloud/hello-spring-cloud-eureka/src/main/java/com/gtstar/hello/spring/cloud/eureka/EurekaApplication.java
+++ code/hello-spring-cloud/hello-spring-cloud-eureka/src/main/java/com/gtstar/hello/spring/cloud/eureka/EurekaApplication.java
@@ -7,7 +7,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-
+	/**
+	 * @Author Allen
+	 * @Description TODO
+	 * @Date 11:44 2019/12/8
+	 * @Param [args]
+	 * @return void
+	 **/
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
