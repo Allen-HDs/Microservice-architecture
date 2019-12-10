bde6b1bebf6f6a5861be638c59de355982f4fd86
Allen <yx_endeavor@163.com>
2019-12-10 16:52:32 +0800

    独特了

---
+++ code/hello-spring-cloud/hello-spring-cloud-admin/src/test/java/com/gtstar/hello/spring/cloud/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtstar.hello.spring.cloud.admin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
