fa3def3964f9b1f0ea67999a7540d2e9111bf13e
Allen <yx_endeavor@163.com>
2019-12-10 15:29:44 +0800

    1

---
+++ code/hello-spring-cloud/hello-spring-cloud-zipkin/src/test/java/com/gtstar/hello/spring/cloud/zipkin/ZipKinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gtstar.hello.spring.cloud.zipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipKinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
